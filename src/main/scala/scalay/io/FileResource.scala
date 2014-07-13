aacf3f3192d47d1294eb07547f80257b0479ff35
Kota Mizushima <mizukota@gmail.com>
2014-07-14 08:50:14 +0900

    Implement unimplemented methods

--- src/main/scala/scalay/io/FileResource.scala
+++ src/main/scala/scalay/io/FileResource.scala
@@ -1,14 +1,10 @@
 package scalay.io
 
-import java.io.{File => JFile, FileInputStream, InputStreamReader, BufferedReader}
-import java.io.Closeable
+import java.io.{File => JFile, _}
+
 
 /**
- * Created with IntelliJ IDEA.
- * User: mizushima
- * Date: 2012/09/10
- * Time: 6:15
- * To change this template use File | Settings | File Templates.
+ * @author Kota Mizushima
  */
 class FileResource (path: String) {
   private[this] val file: JFile = new JFile(path)
@@ -18,12 +14,22 @@ class FileResource (path: String) {
   def isAbsolute: Boolean = file.isAbsolute()
   def isHidden: Boolean = file.isHidden()
 
-  def writeBytes (bytes: Array[Byte]): Unit = sys.error("not implemented yet")
+  def writeBytes (bytes: Array[Byte]): Unit = {
+    open(new BufferedOutputStream(new FileOutputStream(file))){stream =>
+      stream.write(bytes)
+      stream.flush()
+    }
+  }
+
   def writeBytes (byte: Byte): Unit = writeBytes(Array(byte))
   def write(string: String): Unit = writeBytes(string.getBytes())
   def write(string: String, encoding: String): Unit = writeBytes(string.getBytes(encoding))
 
-  def readBytes(): Array[Byte] = sys.error("not implemented yet")
+  def readBytes(): Array[Byte] = {
+    open(new BufferedInputStream(new FileInputStream(file))){stream =>
+      Iterator.continually(stream.read()).takeWhile(_ != -1).map(_.toByte).toArray
+    }
+  }
   def read(): String = new String(readBytes())
   def read(encoding: String): Unit = new String(readBytes(), encoding)
 
